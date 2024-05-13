e940d011b541b2338f129f62c69802114fdf4517
Minibrewe <145697819+Minibrewe@users.noreply.github.com>
2024-05-13 10:13:09 +0200

    Update SCD30.ino
    With motor

--- SCD30.ino
+++ SCD30.ino
@@ -3,6 +3,10 @@
 
 Adafruit_SCD30  scd30;
 
+const int directionPin = 2;
+const int stepPin = 3; 
+int test = 0;
+
 
 void setup(void) {
   Serial.begin(115200);
@@ -25,6 +29,12 @@ void setup(void) {
   Serial.print("Measurement Interval: "); 
   Serial.print(scd30.getMeasurementInterval()); 
   Serial.println(" seconds");
+  
+  //Motor pin setup
+  pinMode(directionPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  digitalWrite(directionPin, LOW);
+  digitalWrite(stepPin, LOW);
 }
 
 void loop() {
@@ -48,6 +58,64 @@ void loop() {
   } else {
     //Serial.println("No data");
   }
+  //Read the string sent from terminal, do what it tells.
+  if(Serial.available() > 0){
+    String data = Serial.readString();
+  
+    if(data.equals("open"))
+    {
+    open();
+    }
+    else if(data.equals("close"))
+    {
+    close();
+    }
+    else{
+    standby();
+   }
+  }
+  standby();
 
   delay(100);
+}
+//The standby / do nothing call
+void standby()
+{
+ digitalWrite(stepPin, LOW);
+ delayMicroseconds(650);
+ digitalWrite(stepPin, LOW);
+ delayMicroseconds(650);
+ test = 2;
+}
+// The driver for the motor
+void run()
+{
+ digitalWrite(stepPin, HIGH);
+ delayMicroseconds(700);
+ digitalWrite(stepPin, LOW);
+ delayMicroseconds(700);
+}
+//Open window
+void open()
+{
+  //Towards stepper motor
+  int i = 0;
+  digitalWrite(directionPin, HIGH);
+  while(i<35000){
+    run();
+    i++;
+  }
+  standby();
+}
+//Close window
+void close()
+{
+  //Towards metal end.
+  int i = 0;
+  digitalWrite(directionPin, LOW);
+  while(i<35000){
+    run();
+    i++;
+  }
+  standby();
 }
