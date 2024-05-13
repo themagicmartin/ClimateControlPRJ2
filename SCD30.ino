19fd1decbb86d9bd0605cd473514ffccd3e78d86
magicmartin <142316740+themagicmartin@users.noreply.github.com>
2024-05-13 19:46:26 +0200

    Update SCD30.ino
    Commas added to the print for easier data-stripping.

--- SCD30.ino
+++ SCD30.ino
@@ -3,9 +3,6 @@
 
 Adafruit_SCD30  scd30;
 
-const int directionPin = 2;
-const int stepPin = 3; 
-
 
 void setup(void) {
   Serial.begin(115200);
@@ -28,12 +25,6 @@ void setup(void) {
   Serial.print("Measurement Interval: "); 
   Serial.print(scd30.getMeasurementInterval()); 
   Serial.println(" seconds");
-  
-  //Motor pin setup
-  pinMode(directionPin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-  digitalWrite(directionPin, LOW);
-  digitalWrite(stepPin, LOW);
 }
 
 void loop() {
@@ -44,77 +35,19 @@ void loop() {
 
     Serial.print("Temperature: ");
     Serial.print(scd30.temperature);
-    Serial.print("\n");
+    Serial.print(",\n");
     
     Serial.print("Relative Humidity: ");
     Serial.print(scd30.relative_humidity);
-    Serial.print("\n");
+    Serial.print(",\n");
     
     Serial.print("CO2: ");
-    Serial.print(scd30.CO2, 3);
-    Serial.print("\n");
+    Serial.print(scd30.CO2);
+    Serial.print(",\n");
     
   } else {
     //Serial.println("No data");
   }
-  //Read the string sent from terminal, do what it tells.
-  if(Serial.available() > 0){
-    String data = Serial.readString();
-  
-    if(data.equals("open"))
-    {
-    open();
-    }
-    else if(data.equals("close"))
-    {
-    close();
-    }
-    else{
-    standby();
-   }
-  }
-  standby();
 
   delay(100);
 }
-//The standby / do nothing call
-void standby()
-{
- digitalWrite(stepPin, LOW);
- delayMicroseconds(600);
- digitalWrite(stepPin, LOW);
- delayMicroseconds(600);
- test = 2;
-}
-void run()
-{
- digitalWrite(stepPin, HIGH);
- delayMicroseconds(100);
- digitalWrite(stepPin, LOW);
- delayMicroseconds(100);
-}
-void open()
-{
-  //Towards metal end
-  int i = 0;
-  int u = 0;
-  digitalWrite(directionPin, HIGH);
-  for(uint16_t i; i<64000; i++)
-  {
-    run();
-  }
-  standby();
-}
-
-void close()
-{
-  //Towards stepper motor
-  int i = 0;
-  digitalWrite(directionPin, LOW);
-    for(uint16_t i; i<64000; i++){
-      run();
-  }
-  standby();
-}
-
-
