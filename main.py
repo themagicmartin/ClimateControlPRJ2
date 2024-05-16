4279c50f3409c7ee747525b2c8b0840f89d5ec4a
magicmartin <142316740+themagicmartin@users.noreply.github.com>
2024-05-16 09:54:18 +0200

    Update main.py
    Sensor 2 (inside) added and code updated to take care of this. 
    
    Minor bugs with the serial reception of data. Seems to be very compact and somewhat messy.

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ serialInst = serial.Serial('/dev/tty.usbmodem1101', 115200)  # Port and baudrate
 time.sleep(2)  # Allow time for Arduino to initialize
 
 # Initialize lists for logging data
-time_log = []
-temp_log = []
-hum_log = []
-CO2_log = []
+time_log_out = []
+temp_log_out = []
+hum_log_out = []
+CO2_log_out = []
+
+# Initialize lists for logging data
+time_log_in = []
+temp_log_in = []
+hum_log_in = []
+CO2_log_in = []
 
 # Initialize variables
-temperature = 0
-humidity = 0
-CO2 = 0
-start_time = time.time()
+temperature = humidity = CO2 = None
+temperature_ref = humidity_ref = CO2_ref = None
+start_time_ref = start_time = time.time()
 
 # Initialize threshold values
 temperature_lower_threshold = 10.0
@@ -48,7 +53,7 @@ color_CO2 = 'gray'
 color_temp = 'gray' 
 
 # Methods
-def save_plot(time_log, temp_log, hum_log, CO2_log, output_path): # Converts plots to PNG. Used in update_plots
+def save_plot(time_log_out, temp_log_in, hum_log_in, CO2_log_in, output_path): # Converts plots to PNG. Used in update_plots
     global temperature_lower_threshold, color_temp, humidity_lower_threshold, humidity_upper_threshold, color_hum_low, color_hum_high, CO2_upper_threshold, color_CO2
     
     # Create a new plot
@@ -56,7 +61,7 @@ def save_plot(time_log, temp_log, hum_log, CO2_log, output_path): # Converts plo
     
     # Temperature plot
     plt.subplot(3, 1, 1)
-    plt.plot([t.strftime('%H:%M:%S') for t in time_log], temp_log, color='red')  # Format time to display hour, minute, and second
+    plt.plot([t.strftime('%H:%M:%S') for t in time_log_out], temp_log_in, color='red')  # Format time to display hour, minute, and second
     plt.axhline(y=temperature_lower_threshold, color=color_temp, linestyle='--')
     plt.xlabel('Time')
     plt.ylabel('Temperature (°C)')
@@ -65,12 +70,12 @@ def save_plot(time_log, temp_log, hum_log, CO2_log, output_path): # Converts plo
     plt.xticks(rotation=0)  # Rotate x-axis labels if needed
     
     # Dynamically adjust the number of ticks on the x-axis for temperature plot
-    num_ticks_temp = min(len(time_log), 10)  # Maximum of 10 ticks
+    num_ticks_temp = min(len(time_log_out), 10)  # Maximum of 10 ticks
     plt.gca().xaxis.set_major_locator(plt.MaxNLocator(num_ticks_temp))
     
     # Humidity plot
     plt.subplot(3, 1, 2)
-    plt.plot([t.strftime('%H:%M:%S') for t in time_log], hum_log, color='blue')  # Format time to display hour, minute, and second
+    plt.plot([t.strftime('%H:%M:%S') for t in time_log_out], hum_log_in, color='blue')  # Format time to display hour, minute, and second
     plt.axhline(y=humidity_lower_threshold, color=color_hum_low, linestyle='--')
     plt.axhline(y=humidity_upper_threshold, color=color_hum_high, linestyle='--')
     plt.xlabel('Time')
@@ -81,12 +86,12 @@ def save_plot(time_log, temp_log, hum_log, CO2_log, output_path): # Converts plo
     plt.xticks(rotation=0)  # Rotate x-axis labels if needed
     
     # Dynamically adjust the number of ticks on the x-axis for humidity plot
-    num_ticks_hum = min(len(time_log), 10)  # Maximum of 10 ticks
+    num_ticks_hum = min(len(time_log_out), 10)  # Maximum of 10 ticks
     plt.gca().xaxis.set_major_locator(plt.MaxNLocator(num_ticks_hum))
     
     # CO2 plot
     plt.subplot(3, 1, 3)
-    plt.plot([t.strftime('%H:%M:%S') for t in time_log], CO2_log, color='green')  # Format time to display hour, minute, and second
+    plt.plot([t.strftime('%H:%M:%S') for t in time_log_out], CO2_log_in, color='green')  # Format time to display hour, minute, and second
     plt.axhline(y=CO2_upper_threshold, color=color_CO2, linestyle='--')
     plt.xlabel('Time')
     plt.ylabel('CO2 (ppm)')
@@ -95,7 +100,7 @@ def save_plot(time_log, temp_log, hum_log, CO2_log, output_path): # Converts plo
     plt.xticks(rotation=0)  # Rotate x-axis labels if needed
     
     # Dynamically adjust the number of ticks on the x-axis for CO2 plot
-    num_ticks_CO2 = min(len(time_log), 10)  # Maximum of 10 ticks
+    num_ticks_CO2 = min(len(time_log_out), 10)  # Maximum of 10 ticks
     plt.gca().xaxis.set_major_locator(plt.MaxNLocator(num_ticks_CO2))
     
     plt.subplots_adjust(hspace=0.5)  # Adjust the vertical spacing between subplots
@@ -108,64 +113,97 @@ def save_plot(time_log, temp_log, hum_log, CO2_log, output_path): # Converts plo
     # Close the current plot to release memory
     plt.close()
 
-
-def log_data_thread(): #Function to log data in a seperate thread
-    global temperature, humidity, CO2, time_log, temp_log, hum_log, CO2_log
+def log_data_thread():
+    global temperature, humidity, CO2
+    global temperature_ref, humidity_ref, CO2_ref
     while True:
-        # Read data from Arduino
-        data = serialInst.readline().decode().strip()  # Read a line of data
-        if data:  # Check for data
-            current_time = datetime.now()
-            lines = data.split('\n')
-            for line in lines:
-                if "Temperature" in line:
-                    temperature = float(line.split(":")[1].split(",")[0])  # Extract the value between ':' and ','
-                elif "Relative Humidity" in line:
-                    humidity = float(line.split(":")[1].split(",")[0])  # Extract the value between ':' and ','
-                elif "CO2" in line:
-                    CO2 = float(line.split(":")[1].split(",")[0])  # Extract the value between ':' and ','
+        try:
+            # Read data from Arduino
+            data = serialInst.readline().decode('utf-8', errors='ignore').strip()
+            if data:  # Check for data
+                current_time = datetime.now()
+                if data.startswith("Temperature: "):
+                    temperature_ref = float(data.split("Temperature: ")[1].split(",")[0])
+                elif data.startswith("Relative Humidity: "):
+                    humidity_ref = float(data.split("Relative Humidity: ")[1].split(",")[0])
+                elif data.startswith("CO2: "):
+                    CO2_ref = float(data.split("CO2: ")[1].split(",")[0])
+                elif data.startswith("Temperature1: "):
+                    temperature = float(data.split("Temperature1: ")[1].split(",")[0])
+                elif data.startswith("Relative Humidity1: "):
+                    humidity = float(data.split("Relative Humidity1: ")[1].split(",")[0])
+                elif data.startswith("CO21: "):
+                    CO2 = float(data.split("CO21: ")[1].split(",")[0])
 
-            # Log received data to lists
-            time_log.append(current_time)  # Append current time as datetime object
-            temp_log.append(temperature)
-            hum_log.append(humidity)
-            CO2_log.append(CO2)
-            
+                # Log received data (from outside) to lists
+                if temperature and humidity and CO2:
+                    time_log_out.append(current_time)
+                    temp_log_out.append(temperature_ref)
+                    hum_log_out.append(humidity_ref)
+                    CO2_log_out.append(CO2_ref)
+                    
+                # Log received data (from inside) to reference lists
+                if temperature_ref and humidity_ref and CO2_ref:
+                    time_log_in.append(current_time)
+                    temp_log_in.append(temperature)
+                    hum_log_in.append(humidity)
+                    CO2_log_in.append(CO2)
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+def generate_averages_text(): # Generates average values for sensor outside
+    # NOT AVERAGES BUT CURRENT VALUES!!
+    avg_temp = temperature_ref  # UPDATE TO AVERAGE
+    avg_hum = humidity_ref      # UPDATE TO AVERAGE
+    avg_CO2 = CO2_ref           # UPDATE TO AVERAGE
+
+    # Create a string with the current time and the average values
+    output_string = f"Current Temperature: {avg_temp} °C\n" \
+                    f"Current Humidity: {avg_hum} %\n" \
+                    f"Current CO2: {avg_CO2} ppm"
+
+    # Save the string to a text file
+    with open('static/averages.txt', 'w') as file:
+        file.write(output_string)
+
 def update_plots(): # Update plots with new data
     # Calculate the current time
     current_time = datetime.now()
     
     # Plot for the last minute
     last_minute_time = current_time - timedelta(minutes=1)
-    mask_minute = [t >= last_minute_time for t in time_log]
-    time_minute = [t for t, mask in zip(time_log, mask_minute) if mask]
-    temp_minute = [temp for temp, mask in zip(temp_log, mask_minute) if mask]
-    hum_minute = [hum for hum, mask in zip(hum_log, mask_minute) if mask]
-    CO2_minute = [CO2 for CO2, mask in zip(CO2_log, mask_minute) if mask]
+    mask_minute = [t >= last_minute_time for t in time_log_out]
+    time_minute = [t for t, mask in zip(time_log_out, mask_minute) if mask]
+    temp_minute = [temp for temp, mask in zip(temp_log_in, mask_minute) if mask]
+    hum_minute = [hum for hum, mask in zip(hum_log_in, mask_minute) if mask]
+    CO2_minute = [CO2 for CO2, mask in zip(CO2_log_in, mask_minute) if mask]
     if CO2_minute:  # Check if CO2_minute list is not empty
         save_plot(time_minute, temp_minute, hum_minute, CO2_minute, 'static/images/minute.png')
     
     # Plot for the last hour
     last_hour_time = current_time - timedelta(hours=1)
-    mask_hourly = [t >= last_hour_time for t in time_log]
-    time_hourly = [t for t, mask in zip(time_log, mask_hourly) if mask]
-    temp_hourly = [temp for temp, mask in zip(temp_log, mask_hourly) if mask]
-    hum_hourly = [hum for hum, mask in zip(hum_log, mask_hourly) if mask]
-    CO2_hourly = [CO2 for CO2, mask in zip(CO2_log, mask_hourly) if mask]
+    mask_hourly = [t >= last_hour_time for t in time_log_out]
+    time_hourly = [t for t, mask in zip(time_log_out, mask_hourly) if mask]
+    temp_hourly = [temp for temp, mask in zip(temp_log_in, mask_hourly) if mask]
+    hum_hourly = [hum for hum, mask in zip(hum_log_in, mask_hourly) if mask]
+    CO2_hourly = [CO2 for CO2, mask in zip(CO2_log_in, mask_hourly) if mask]
     if CO2_hourly:  # Check if CO2_hourly list is not empty
         save_plot(time_hourly, temp_hourly, hum_hourly, CO2_hourly, 'static/images/hourly.png')
     
     # Plot for the last 24 hours
     last_24_hours_time = current_time - timedelta(days=1)
-    mask_daily = [t >= last_24_hours_time for t in time_log]
-    time_daily = [t for t, mask in zip(time_log, mask_daily) if mask]
-    temp_daily = [temp for temp, mask in zip(temp_log, mask_daily) if mask]
-    hum_daily = [hum for hum, mask in zip(hum_log, mask_daily) if mask]
-    CO2_daily = [CO2 for CO2, mask in zip(CO2_log, mask_daily) if mask]
+    mask_daily = [t >= last_24_hours_time for t in time_log_out]
+    time_daily = [t for t, mask in zip(time_log_out, mask_daily) if mask]
+    temp_daily = [temp for temp, mask in zip(temp_log_in, mask_daily) if mask]
+    hum_daily = [hum for hum, mask in zip(hum_log_in, mask_daily) if mask]
+    CO2_daily = [CO2 for CO2, mask in zip(CO2_log_in, mask_daily) if mask]
     if CO2_daily:  # Check if CO2_daily list is not empty
         save_plot(time_daily, temp_daily, hum_daily, CO2_daily, 'static/images/daily.png')
 
-    time.sleep(2)  # Adjust the sleep time as needed
+    generate_averages_text()
+
+    time.sleep(10)  # Adjust the sleep time as needed
 
 def check_triggers(): # Compare data to thresholds
     global temperature, humidity, CO2, CO2_beyond, hum_below, hum_beyond, temp_below, color_CO2, color_hum_low, color_hum_high, color_temp
@@ -222,20 +260,16 @@ def open_window():
     global WindowIsOpen
     print("UPDATE: Opening window!")
     # CODE HERE
-    message = "open\n"
+    message = "j\n"
     serialInst.write(message.encode())
-
-    time.sleep(5)  # Allow time for Window to close
     WindowIsOpen = True
 
 def close_window():
     global WindowIsOpen
     print("UPDATE: Closing window!")
     # CODE HERE
-    message = "close\n"
+    message = "n\n"
     serialInst.write(message.encode())
-
-    time.sleep(5)  # Allow time for Window to close
     WindowIsOpen = False
 
 # Start a thread to run the update_plots_thread function
